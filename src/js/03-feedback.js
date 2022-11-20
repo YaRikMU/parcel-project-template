0094ac1d879fdd0a9cd2e4794adf80801bd6548c
Yaroslav Mulyk <Scofild540@gmail.com>
2022-11-20 22:01:05 +0200

    12

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,3 +5,22 @@ const formEl = document.querySelector('.feedback-form');
 
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+
+function onFormSubmit (evt) {
+evt.preventDefault();
+console.log(evt)
+if (!evt.target.email.value || !evt.target.message.value) {
+    alert('Enter all data');
+    return;
+  }
+}
+
+
+function onFormInput (e) {
+
+    console.log(e.target.name);
+    console.log(e.target.value);
+}
